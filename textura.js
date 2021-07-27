43c4d20015626054c45f38e039ea173c44778598
Josué Romero <79048209+josueromram@users.noreply.github.com>
2021-07-26 23:11:08 -0500

    Add files via upload

---
+++ textura.js
@@ -0,0 +1,13 @@
+pregunta = prompt("¿Quieres continuar?");
+
+if (pregunta == 'si') {
+	alert("Gracias, dale click a ACEPTAR para ver la plataforma");
+} else if (pregunta == 'no') {
+	confirm("Haz perdido tu tiempo, jajaja porque no te dejaré ir");
+} else if (pregunta == 'SI') {
+	alert("Gracias, dale click a ACEPTAR para ver la plataforma");
+}else if (pregunta == 'NO') {
+	confirm("Haz perdido tu tiempo, jajaja porque no te dejaré ir");
+}else {
+	alert("NO SEA TÚ TAN HP OMBE.")
+}
